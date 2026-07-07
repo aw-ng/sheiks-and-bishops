a489d238dcfc300598028791ddfd25ce46345780
badasswp <badasswpdev@gmail.com>
2026-07-07 08:38:14 +0100

    chore: add js scripts

--- scripts/pages/home.js
+++ scripts/pages/home.js
@@ -3,7 +3,85 @@
  */
 (() => {
 	document.addEventListener('DOMContentLoaded', function () {
-		// JavaScript code goes here
-		console.log('Home Page');
+		loadSlider();
 	});
+
+	/**
+	 * Load Slider.
+	 *
+	 * Displays the brand logo sliding
+	 * from right to left.
+	 *
+	 * @returns {void}
+	 */
+	const loadSlider = () => {
+		const brandSlider = document.getElementById('brand-slider');
+		const clienteleSlider = document.getElementById('clientele-slider');
+		const heroSlider = document.getElementById('hero-slider');
+
+		if (brandSlider) {
+			new Splide('#brand-slider', {
+				type: 'loop',
+				drag: 'free',
+				focus: 'center',
+				perPage: 5,
+				autoScroll: { speed: 0.25 },
+				arrows: false,
+				pagination: false,
+				breakpoints: {
+					991: {
+						perPage: 3
+					}
+				}
+			}).mount(window.splide.Extensions);
+		}
+
+		if (clienteleSlider) {
+			const clientele = new Splide('#clientele-slider', {
+				drag: 'free',
+				perPage: 4,
+				perMove: 1,
+				gap: '3rem',
+				arrows: false,
+				pagination: false,
+				type: 'loop',
+				autoplay: true,
+				interval: 5000,
+				breakpoints: {
+					786: {
+						perPage: 1
+					},
+					991: {
+						perPage: 2
+					},
+					1366: {
+						perPage: 3
+					}
+				}
+			});
+
+			clientele.mount();
+
+			document
+				.getElementById('clientele-prev')
+				.addEventListener('click', () => clientele.go('<'));
+
+			document
+				.getElementById('clientele-next')
+				.addEventListener('click', () => clientele.go('>'));
+		}
+
+		if (heroSlider) {
+			new Splide('#hero-slider', {
+				drag: 'free',
+				focus: 'center',
+				perPage: 1,
+				arrows: false,
+				pagination: false,
+				type: 'loop',
+				autoplay: true,
+				interval: 10000
+			}).mount();
+		}
+	};
 })();
